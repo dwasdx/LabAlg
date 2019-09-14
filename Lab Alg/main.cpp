2795c3da6b37aac1b9af2c428c11a218fccfb9e0
Andrei Zhuravlev <dwasdx@gmail.com>
2019-09-14 19:09:03 +0300

    Initial Commit

---
+++ Lab Alg/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab Alg
+//
+//  Created by Андрей Журавлев on 14/09/2019.
+//  Copyright © 2019 Андрей Журавлев. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
